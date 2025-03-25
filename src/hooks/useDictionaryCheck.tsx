3a64289b3298e6676e7483efd12896b30de59b1c
Jacobs2Jacob <30956957+Jacobs2Jacob@users.noreply.github.com>
2025-03-25 06:15:21 +0200

    Update useDictionaryCheck.tsx

--- src/hooks/useDictionaryCheck.tsx
+++ src/hooks/useDictionaryCheck.tsx
@@ -12,25 +12,21 @@ export const useDictionaryCheck = (minWordLength: number) => {
         // if no characters at all
         if (!word) {
             setExists(null);
-            return;
         }
-
         // at least one character but not all is filled
-        if (word.length < minWordLength) {
+        else if (word.length < minWordLength) {
             setExists(false);
-            return;
         }
-
         // prevent calling api again on same value
-        if (lastChecked && word === lastChecked.val) {
+        else if (lastChecked && word === lastChecked.val) {
             setExists(lastChecked.exists);
-            return;
         }
-
+        else
+        {
         // for loader if needed
-        setIsChecking(true);
+            setIsChecking(true);
 
-        fetch(`${apiUrl}/${word}`)
+            fetch(`${apiUrl}/${word}`)
             .then(res => {
                 setExists(res.ok);
                 setLastChecked({ val: word, exists: res.ok });
@@ -41,6 +37,7 @@ export const useDictionaryCheck = (minWordLength: number) => {
                 setLastChecked({ val: word, exists: false });
             })
             .finally(() => setIsChecking(false));
+        }
     }, [lastChecked, apiUrl, minWordLength]);
 
     // use debounce for keypress delay
